728d91442119f50c3e505ba2f73fcbcd1c933320
Kateryna Melnyk <kattemelnyk@gmail.com>
2026-09-20 16:49:48 +0300

    Address Unit 10 review comments

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+      baseURL: 'https://practicesoftwaretesting.com',
+testIdAttribute: 'data-test',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -73,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
+  //   url: 'https://practicesoftwaretesting.com',
   //   reuseExistingServer: !process.env.CI,
   // },
 });
